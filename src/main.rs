9b0e677cbb4af132ff432c77ae991554bda6c12b
Jeremiah Russell <jerry@jrussell.ie>
2025-03-22 08:11:14 +0000

    ✨ feat(main): integrate word processing module
    - add slb::Words module for word processing
    - set source directory and file for word data
    - initialize environment logger for better debugging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use slb::Words;
+
+const SOURCE_DIR: &str = "words";
+const SOURCE_FILE: &str = "mit_words.txt";
+
 fn main() {
     println!("Hello, world!");
+
+    env_logger::init();
+
+    let src = format!("{SOURCE_DIR}/{SOURCE_FILE}");
+
+    let _words = Words::new(&src);
+
 }
