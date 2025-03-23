90ef3a1c211b3e25cd1830ede2b0427cd8ce1948
Jeremiah Russell <jerry@jrussell.ie>
2025-03-23 07:20:51 +0000

    ✨ feat(main): implement word filtering and logging
    - replace Words with PrepareWords for improved word preparation
    - add tracing and logging functionality for better debugging
    - introduce default constants for source directory, file, and minimum word length
    - implement word filtering by minimum length and filtering out repeated letters
    
    ♻️ refactor(main): enhance logging setup
    
    - replace env_logger with tracing for more flexible logging
    - create get_logging function to configure logging level and subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,55 @@
-use slb::Words;
+use slb::PrepareWords;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
-const SOURCE_DIR: &str = "words";
-const SOURCE_FILE: &str = "mit_words.txt";
+const DEFAULT_SOURCE_DIR: &str = "words";
+const DEFAULT_SOURCE_FILE: &str = "mit_words.txt";
+const DEFAULT_MINIMUM_WORD_LENGTH: usize = 3;
 
 fn main() {
     println!("Hello, world!");
+    // Setup logging/tracing
 
-    env_logger::init();
+    get_logging(log::LevelFilter::Debug);
 
-    let src = format!("{SOURCE_DIR}/{SOURCE_FILE}");
+    // Setup settings
+    let src_directory = DEFAULT_SOURCE_DIR;
+    let src_file = DEFAULT_SOURCE_FILE;
+    let minimum_word_length = DEFAULT_MINIMUM_WORD_LENGTH;
 
-    let _words = Words::new(&src);
+    let src = format!("{src_directory}/{src_file}");
 
+    let words: Vec<String> = std::fs::read_to_string(src)
+        .expect("Failed to read words file")
+        .lines()
+        .map(|line| line.trim().to_string())
+        .collect();
+
+    tracing::info!("Loaded {} words", words.len());
+    let words = words
+        .filter_to_minimum_length(minimum_word_length)
+        .filter_no_repeated_letters();
+
+    tracing::info!("Filtered words includes {} words", words.len());
+}
+
+pub fn get_logging(verbosity: log::LevelFilter) {
+    let filter = EnvFilter::from(format!(
+        "slb={}",
+        if verbosity == log::LevelFilter::Trace {
+            log::LevelFilter::Debug
+        } else {
+            verbosity
+        }
+    ));
+
+    let log_subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .pretty()
+        .with_env_filter(filter)
+        .finish();
+
+    let _ = tracing::subscriber::set_global_default(log_subscriber)
+        .map_err(|_| eprintln!("Unable to set global default subscriber!"));
+
+    info!("Initialised logging to console at {verbosity}");
 }
