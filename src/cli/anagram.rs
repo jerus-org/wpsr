e6456c487b756bddca603defde4d820ccc9d3ff3
Jeremiah Russell <jerry@jrussell.ie>
2025-04-09 15:36:41 +0100

    ✨ feat(cli): add anagram command support
    - implement Cmd struct for handling anagram command line arguments
    - add run method to execute the anagram solution process
    - support directory and file options for word list sources

---
+++ src/cli/anagram.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+
+use crate::{Anagram, Error};
+use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+pub struct Cmd {
+    pub letters: String,
+    /// word list source directory
+    #[arg(short, long)]
+    pub dir: Option<String>,
+    /// word list source file
+    #[arg(short, long)]
+    pub file: Option<String>,
+}
+
+impl Cmd {
+    #[tracing::instrument(skip(self, settings))]
+    pub fn run(&self, settings: HashMap<String, String>) -> Result<(), Error> {
+        tracing::debug!("Args: {self:#?}");
+
+        let mut solution = Anagram::new(&self.letters, settings)?;
+        solution
+            .set_word_source(self.dir.clone(), self.file.clone())
+            .load_words()
+            .find_solutions()?;
+
+        println!("{}", solution.solutions_title());
+        println!("{}\n", solution.word_source_string());
+        println!("{}", solution.distribution_string());
+        println!("{}", solution.solutions_string());
+
+        Ok(())
+    }
+}
